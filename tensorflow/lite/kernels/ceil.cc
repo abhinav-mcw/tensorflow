bb40eab497e1610093461036e69466d05e2494bc
abhinav-mcw <abhinav.pharswan@multicorewareinc.com>
2024-07-30 17:00:36 +0530

    Tflite ceil missing datatypes support -Adds i8,i16,bf16,f16 for tflite ceil. -Adds i8,i16,bf16,f16 ceil unit tests.

--- tensorflow/lite/kernels/ceil.cc
+++ tensorflow/lite/kernels/ceil.cc
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/lite/core/c/common.h"
+#include "tensorflow/lite/kernels/dequantize.h"
 #include "tensorflow/lite/kernels/internal/optimized/optimized_ops.h"
 #include "tensorflow/lite/kernels/internal/tensor.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
@@ -25,10 +26,56 @@ namespace ops {
 namespace builtin {
 namespace ceil {
 
+const int kTensorNotAllocated = -1;
 constexpr int kInputTensor = 0;
 constexpr int kOutputTensor = 0;
+struct OpData {
+  int input_dequantized_id = kTensorNotAllocated;
+  int output_dequantized_id = kTensorNotAllocated;
+
+  int input_dequantized_index;
+  int output_dequantized_index;
+};
+
+void* Init(TfLiteContext* context, const char* buffer, size_t length) {
+  auto* data = new OpData;
+  return data;
+}
+
+void Free(TfLiteContext* context, void* buffer) {
+  delete reinterpret_cast<OpData*>(buffer);
+}
+
+TfLiteStatus AllocateTemporaryTensorsIfRequired(TfLiteContext* context,
+                                                TfLiteNode* node,
+                                                bool is_quantized) {
+  OpData* data = reinterpret_cast<OpData*>(node->user_data);
+
+  int temporaries_count = 0;
+
+  if (is_quantized) {
+    data->input_dequantized_index = temporaries_count;
+    if (data->input_dequantized_id == kTensorNotAllocated) {
+      TF_LITE_ENSURE_OK(context, context->AddTensors(
+                                     context, 1, &data->input_dequantized_id));
+    }
+    temporaries_count++;
+
+    data->output_dequantized_index = temporaries_count;
+    if (data->output_dequantized_id == kTensorNotAllocated) {
+      TF_LITE_ENSURE_OK(context, context->AddTensors(
+                                     context, 1, &data->output_dequantized_id));
+    }
+    temporaries_count++;
+  }
+  TfLiteIntArrayFree(node->temporaries);
+  node->temporaries = TfLiteIntArrayCreate(temporaries_count);
+
+  return kTfLiteOk;
+}
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
+  OpData* data = reinterpret_cast<OpData*>(node->user_data);
   const TfLiteTensor* input;
   TF_LITE_ENSURE_OK(context, GetInputSafe(context, node, kInputTensor, &input));
   TfLiteTensor* output;
@@ -36,32 +83,165 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
                     GetOutputSafe(context, node, kOutputTensor, &output));
   TF_LITE_ENSURE_EQ(context, NumInputs(node), 1);
   TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
-  TF_LITE_ENSURE_TYPES_EQ(context, input->type, kTfLiteFloat32);
+  TF_LITE_ENSURE(
+      context,
+      input->type == kTfLiteFloat32 || input->type == kTfLiteBFloat16 ||
+          input->type == kTfLiteFloat16 || input->type == kTfLiteInt32 ||
+          input->type == kTfLiteInt16 || input->type == kTfLiteInt8);
+
+  bool is_quantized = input->quantization.type != kTfLiteNoQuantization;
+
+  if (input->type == kTfLiteInt8 ||
+      (input->type == kTfLiteInt16 && is_quantized)) {
+    const auto* input_params =
+        reinterpret_cast<TfLiteAffineQuantization*>(input->quantization.params);
+    const auto* output_params = reinterpret_cast<TfLiteAffineQuantization*>(
+        output->quantization.params);
+
+    TF_LITE_ENSURE(context, input_params != nullptr);
+    TF_LITE_ENSURE(context, input_params->scale != nullptr);
+    TF_LITE_ENSURE(context, input_params->scale->size > 0);
+    TF_LITE_ENSURE(context, input_params->zero_point->size > 0);
+
+    TF_LITE_ENSURE(context, output_params != nullptr);
+    TF_LITE_ENSURE(context, output_params->scale != nullptr);
+    TF_LITE_ENSURE(context, output_params->scale->size > 0);
+    TF_LITE_ENSURE(context, output_params->zero_point->size > 0);
+
+    if (input->type == kTfLiteInt16) {
+      // In case of int16, quantization is symmetic and
+      // zero point should be zero.
+      TF_LITE_ENSURE_EQ(context, input->params.zero_point, 0);
+      TF_LITE_ENSURE_EQ(context, output->params.zero_point, 0);
+    }
+  }
+  TF_LITE_ENSURE_STATUS(
+      AllocateTemporaryTensorsIfRequired(context, node, is_quantized));
+
   output->type = input->type;
   TfLiteIntArray* output_size = TfLiteIntArrayCopy(input->dims);
+
+  if (is_quantized) {
+    node->temporaries->data[data->input_dequantized_index] =
+        data->input_dequantized_id;
+    TfLiteTensor* input_dequantized;
+    TF_LITE_ENSURE_OK(
+        context, GetTemporarySafe(context, node, data->input_dequantized_index,
+                                  &input_dequantized));
+    input_dequantized->type = kTfLiteFloat32;
+    input_dequantized->allocation_type = kTfLiteArenaRw;
+    if (!TfLiteIntArrayEqual(input_dequantized->dims, input->dims)) {
+      TfLiteIntArray* input_dequantized_size = TfLiteIntArrayCopy(input->dims);
+      TF_LITE_ENSURE_OK(context,
+                        context->ResizeTensor(context, input_dequantized,
+                                              input_dequantized_size));
+    }
+    node->temporaries->data[data->output_dequantized_index] =
+        data->output_dequantized_id;
+    TfLiteTensor* output_dequantized;
+    TF_LITE_ENSURE_OK(
+        context, GetTemporarySafe(context, node, data->output_dequantized_index,
+                                  &output_dequantized));
+    output_dequantized->type = kTfLiteFloat32;
+    output_dequantized->allocation_type = kTfLiteArenaRw;
+    if (!TfLiteIntArrayEqual(output_dequantized->dims, output->dims)) {
+      TfLiteIntArray* output_dequantized_size = TfLiteIntArrayCopy(input_dequantized->dims);
+      TF_LITE_ENSURE_OK(context,
+                        context->ResizeTensor(context, output_dequantized,
+                                              output_dequantized_size));
+    }
+  }
+
   return context->ResizeTensor(context, output, output_size);
 }
 
+template <typename T>
+TfLiteStatus EvalCeil(const TfLiteTensor* input, TfLiteTensor* output) {
+  optimized_ops::Ceil<T>(GetTensorShape(input), GetTensorData<T>(input),
+                         GetTensorShape(output), GetTensorData<T>(output));
+  return TfLiteStatus::kTfLiteOk;
+}
+
+template <typename T>
+TfLiteStatus EvalCeilQuantized(TfLiteContext* context, TfLiteNode* node,
+                               OpData* data, const TfLiteTensor* input,
+                               TfLiteTensor* output) {
+  TfLiteTensor* dequantized_input_tensor;
+  TF_LITE_ENSURE_OK(
+      context, GetTemporarySafe(context, node, data->input_dequantized_index,
+                                &dequantized_input_tensor));
+  ;
+  TfLiteTensor* dequantized_output_tensor;
+  TF_LITE_ENSURE_OK(context,
+                    GetTemporarySafe(context, node, data->output_dequantized_index,
+                                  &dequantized_output_tensor));
+
+  dequantize::DequantizeImpl<dequantize::KernelType::kGenericOptimized>(
+      context, node, input, dequantized_input_tensor);
+
+  TF_LITE_ENSURE_OK(context, EvalCeil<float>(dequantized_input_tensor,
+                                             dequantized_output_tensor));
+
+  RuntimeShape output_shape(GetTensorShape(output));
+  RuntimeShape dequantized_output_shape(
+      GetTensorShape(dequantized_output_tensor));
+
+  tflite::QuantizationParams op_params;
+  op_params.zero_point = output->params.zero_point;
+  op_params.scale = output->params.scale;
+  optimized_ops::AffineQuantize<T>(
+      op_params, dequantized_output_shape,
+      GetTensorData<float>(dequantized_output_tensor), output_shape,
+      GetTensorData<T>(output));
+  return TfLiteStatus::kTfLiteOk;
+}
+
 TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
+  OpData* data = reinterpret_cast<OpData*>(node->user_data);
   const TfLiteTensor* input;
   TF_LITE_ENSURE_OK(context, GetInputSafe(context, node, kInputTensor, &input));
   TfLiteTensor* output;
   TF_LITE_ENSURE_OK(context,
                     GetOutputSafe(context, node, kOutputTensor, &output));
-  if (input->type != kTfLiteFloat32) {
-    TF_LITE_UNSUPPORTED_TYPE(context, input->type, "Ceil");
+
+  switch (input->type) {
+    case kTfLiteFloat32:
+      TF_LITE_ENSURE_OK(context, EvalCeil<float>(input, output));
+      break;
+    case kTfLiteFloat16:
+      TF_LITE_ENSURE_OK(context, EvalCeil<Eigen::half>(input, output));
+      break;
+    case kTfLiteBFloat16:
+      TF_LITE_ENSURE_OK(context, EvalCeil<Eigen::bfloat16>(input, output));
+      break;
+    case kTfLiteInt32:
+      TF_LITE_ENSURE_OK(context, EvalCeil<int32_t>(input, output));
+      break;
+    case kTfLiteInt8:
+      TF_LITE_ENSURE_OK(context, EvalCeilQuantized<int8_t>(context, node, data,
+                                                           input, output));
+      break;
+    case kTfLiteInt16: {
+      if (output->quantization.type == kTfLiteNoQuantization)
+        TF_LITE_ENSURE_OK(context, EvalCeil<int16_t>(input, output));
+      else
+        TF_LITE_ENSURE_OK(context, EvalCeilQuantized<int16_t>(
+                                       context, node, data, input, output));
+    } break;
+    default: {
+      TF_LITE_KERNEL_LOG(context, "Unsupported datatype for ceil output: %s",
+                         TfLiteTypeGetName(output->type));
+      return TfLiteStatus::kTfLiteError;
+    }
   }
-
-  optimized_ops::Ceil(GetTensorShape(input), GetTensorData<float>(input),
-                      GetTensorShape(output), GetTensorData<float>(output));
-
   return kTfLiteOk;
 }
 }  // namespace ceil
 
 TfLiteRegistration* Register_CEIL() {
-  static TfLiteRegistration r = {/*init=*/nullptr,
-                                 /*free=*/nullptr, ceil::Prepare, ceil::Eval};
+  static TfLiteRegistration r = {/*init=*/ceil::Init,
+                                 /*free=*/ceil::Free, ceil::Prepare,
+                                 ceil::Eval};
   return &r;
 }
 
--- tensorflow/lite/kernels/ceil_test.cc
+++ tensorflow/lite/kernels/ceil_test.cc
@@ -26,37 +26,169 @@ namespace {
 
 using ::testing::ElementsAreArray;
 
-class CeilOpModel : public SingleOpModel {
+class BaseCeilOpModel : public SingleOpModel {
  public:
-  CeilOpModel(std::initializer_list<int> input_shape, TensorType input_type) {
-    input_ = AddInput(TensorType_FLOAT32);
-    output_ = AddOutput(TensorType_FLOAT32);
+  BaseCeilOpModel(const TensorData& input, const TensorData& output) {
+    input_ = AddInput(input);
+    output_ = AddOutput(output);
     SetBuiltinOp(BuiltinOperator_CEIL, BuiltinOptions_NONE, 0);
-    BuildInterpreter({
-        input_shape,
-    });
+    BuildInterpreter({GetShape(input_)});
   }
-
   int input() { return input_; }
 
-  std::vector<float> GetOutput() { return ExtractVector<float>(output_); }
   std::vector<int> GetOutputShape() { return GetTensorShape(output_); }
 
- private:
+ protected:
   int input_;
   int output_;
 };
 
-TEST(CeilOpTest, SingleDim) {
-  CeilOpModel model({2}, TensorType_FLOAT32);
-  model.PopulateTensor<float>(model.input(), {8.5, 0.0});
-  ASSERT_EQ(model.Invoke(), kTfLiteOk);
-  EXPECT_THAT(model.GetOutput(), ElementsAreArray({9, 0}));
-  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2}));
+class FloatCeilOpModel : public BaseCeilOpModel {
+ public:
+  using BaseCeilOpModel::BaseCeilOpModel;
+
+  template <typename T>
+  std::vector<T> GetOutput() {
+    return ExtractVector<T>(output_);
+  }
+};
+
+class IntegerCeilOpmodel : public BaseCeilOpModel {
+ public:
+  using BaseCeilOpModel::BaseCeilOpModel;
+
+  template <typename T>
+  std::vector<T> GetOutput() {
+    return ExtractVector<T>(output_);
+  }
+};
+
+class QuantizedCeilOpModel : public BaseCeilOpModel {
+ public:
+  QuantizedCeilOpModel(TensorData input, TensorData output)
+      : BaseCeilOpModel(SymmetricInt16Scaling(std::move(input)),
+                        SymmetricInt16Scaling(std::move(output))) {}
+
+  // std::vector<float> GetOutput() { return ExtractVector<float>(output_); }
+  template <typename integer_dtype>
+  std::vector<float> GetDequantizedOutput() {
+    return Dequantize<integer_dtype>(ExtractVector<integer_dtype>(output_),
+                                     GetScale(output_), GetZeroPoint(output_));
+  }
+
+ private:
+  TensorData SymmetricInt16Scaling(TensorData tensor) {
+    // Symmetric range and null zero-point is required for INT16 tensors. As
+    // SingleOpModel::QuantizationParams calculates the scale on an asymmetric
+    // base [int_type::min, int_type::max], manually calculate the scale on a
+    // symmetric range [int_type::min+1, int_type::max] to ensure a null
+    // zero-point.
+    if (tensor.type == TensorType_INT16) {
+      CHECK_EQ(std::abs(tensor.min), tensor.max);
+      tensor.scale = tensor.max / std::numeric_limits<int16_t>::max();
+      tensor.zero_point = 0;
+      tensor.min = 0;
+      tensor.max = 0;
+    }
+
+    return tensor;
+  }
+};
+
+template <typename T>
+float GetTolerance(float min, float max) {
+  float kQuantizedStep = (max - min) / (std::numeric_limits<T>::max() -
+                                        std::numeric_limits<T>::min());
+  return kQuantizedStep;
 }
 
-TEST(CeilOpTest, MultiDims) {
-  CeilOpModel model({2, 1, 1, 5}, TensorType_FLOAT32);
+template <TensorType tensor_type, typename integer_dtype>
+void QuantizedTestsNoActivation() {
+  float kQuantizedTolerance = GetTolerance<integer_dtype>(-7.0, 7.0);
+
+  std::vector<float> input = {-6.2, -5.3, 0, 1.2, 4.4, 6.0};
+
+  std::vector<float> result = {-6.0, -5.0, 0, 2.0, 5.0, 6.0};
+
+  QuantizedCeilOpModel m({tensor_type, {6}, -7.0, 7.0},
+                         {tensor_type, {}, -7.0, 7.0});
+
+  m.QuantizeAndPopulate<integer_dtype>(m.input(), input);
+
+  ASSERT_EQ(m.Invoke(), kTfLiteOk);
+
+  EXPECT_THAT(m.GetDequantizedOutput<integer_dtype>(),
+              ElementsAreArray(ArrayFloatNear(result, kQuantizedTolerance)));
+}
+
+TEST(QuantizedCeilOpModel, QuantizedTestsNoActivationInt8) {
+  QuantizedTestsNoActivation<TensorType_INT8, int8_t>();
+}
+
+TEST(QuantizedCeilOpModel, QuantizedTestsNoActivationInt16) {
+  QuantizedTestsNoActivation<TensorType_INT16, int16_t>();
+}
+
+TEST(QuantizedCeilOpModel, QuantizedTestsNoActivationInt8DifferentMinMax) {
+  float kQuantizedTolerance = GetTolerance<int8_t>(-20.0, 15.0);
+
+  std::vector<float> input = {-6.4, -12.0, 0, -19.0, 4.8, 15.0};
+
+  std::vector<float> result = {-6.0, -12.0, 0, -19.0, 5.0, 15.0};
+
+  QuantizedCeilOpModel m({TensorType_INT8, {6}, -19.0, 15.0},
+                         {TensorType_INT8, {}, -19.0, 15.0});
+
+  m.QuantizeAndPopulate<int8_t>(m.input(), input);
+
+  ASSERT_EQ(m.Invoke(), kTfLiteOk);
+
+  EXPECT_THAT(m.GetDequantizedOutput<int8_t>(),
+              ElementsAreArray(ArrayFloatNear(result, kQuantizedTolerance)));
+}
+
+TEST(CeilOpTest, SingleDimFloat32) {
+  FloatCeilOpModel model({TensorType_FLOAT32, {2}}, {TensorType_FLOAT32, {}});
+  model.PopulateTensor<float>(model.input(), {8.52, 2.02});
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput<float>(),
+              ElementsAreArray(ArrayFloatNear({9.0, 3.0})));
+}
+
+TEST(CeilOpTest, SingleDimFloat16) {
+  FloatCeilOpModel model({TensorType_FLOAT16, {6}}, {TensorType_FLOAT16, {}});
+  std::vector<Eigen::half> input_data{
+      Eigen::half{-535.54f}, Eigen::half{-100.0f}, Eigen::half{-1.0f},
+      Eigen::half{0.f},      Eigen::half{1.0f},    Eigen::half{100.32f}};
+  model.PopulateTensor<Eigen::half>(model.input(), input_data);
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput<Eigen::half>(),
+              ElementsAreArray({-535.0f, -100.0f, -1.0f, 0.f, 1.0f, 101.0f}));
+}
+
+TEST(CeilOpTest, SingleDimBFloat16) {
+  FloatCeilOpModel model({TensorType_BFLOAT16, {2}}, {TensorType_BFLOAT16, {}});
+
+  std::vector<Eigen::bfloat16> input_data{Eigen::bfloat16{2.0f},
+                                          Eigen::bfloat16{3.2f}};
+
+  model.PopulateTensor<Eigen::bfloat16>(model.input(), input_data);
+
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput<Eigen::bfloat16>(),
+              ElementsAreArray({2.0f, 4.0f}));
+}
+
+TEST(IntegerCeilOpTest, SingleDimInteger32) {
+  IntegerCeilOpmodel model({TensorType_INT32, {2}}, {TensorType_INT32, {}});
+  model.PopulateTensor<int32_t>(model.input(), {8, 9});
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput<int32_t>(), ElementsAreArray({8, 9}));
+}
+
+TEST(CeilOpTest, MultiDimsFloat32) {
+  FloatCeilOpModel model({TensorType_FLOAT32, {2, 1, 1, 5}},
+                         {TensorType_FLOAT32, {}});
   model.PopulateTensor<float>(model.input(), {
                                                  0.0001,
                                                  8.0001,
@@ -70,9 +202,43 @@ TEST(CeilOpTest, MultiDims) {
                                                  -0.5,
                                              });
   ASSERT_EQ(model.Invoke(), kTfLiteOk);
-  EXPECT_THAT(model.GetOutput(),
+  EXPECT_THAT(model.GetOutput<float>(),
               ElementsAreArray({1, 9, 1, 10, 1, 0, -8, 0, -9, 0}));
-  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2, 1, 1, 5}));
+}
+
+TEST(CeilOpTest, MultiDimsFloat16) {
+  FloatCeilOpModel model({TensorType_FLOAT16, {2, 1}},
+                         {TensorType_FLOAT16, {}});
+  std::vector<Eigen::half> input_data{Eigen::half{-80.230f},
+                                      Eigen::half{32.3222134f}};
+  model.PopulateTensor<Eigen::half>(model.input(), input_data);
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput<Eigen::half>(),
+              ElementsAreArray({-80.0f, 33.0f}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2, 1}));
+}
+
+TEST(CeilOpTest, MultiDimsBFloat16) {
+  FloatCeilOpModel model({TensorType_BFLOAT16, {2, 2}},
+                         {TensorType_BFLOAT16, {}});
+  std::vector<Eigen::bfloat16> input_data{
+      Eigen::bfloat16{-32.230f}, Eigen::bfloat16{-100.3222134f},
+      Eigen::bfloat16{19.230f}, Eigen::bfloat16{35.32134f}};
+  model.PopulateTensor<Eigen::bfloat16>(model.input(), input_data);
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput<Eigen::bfloat16>(),
+              ElementsAreArray({-32.0f, -100.0f, 20.0f, 36.0f}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2, 2}));
+}
+
+TEST(CeilOpTest, MultiDimsInteger32) {
+  IntegerCeilOpmodel model({TensorType_INT32, {2, 3}}, {TensorType_INT32, {}});
+  model.PopulateTensor<int32_t>(model.input(),
+                                {-1, -1000, -1500000, 0, 450, 1000});
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput<int32_t>(),
+              ElementsAreArray({-1, -1000, -1500000, 0, 450, 1000}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2, 3}));
 }
 
 }  // namespace
